5df8a510f2f0f82402e153edf38cfc446e8e047f
Miguel Bittar <miguelbittar29@gmail.com>
2025-08-20 14:32:00 -0300

    feat: create NurseJoy entity and add JWT dependencies

---
+++ src/main/java/com/hoenn/pokemoncenter/pokemon_center_api/model/NurseJoy.java
@@ -0,0 +1,81 @@
+package com.hoenn.pokemoncenter.pokemon_center_api.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class NurseJoy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    @NotBlank(message = "Username is required!")
+    private String username;
+
+    @NotBlank(message = "Every Nurse needs a password!")
+    private String password;
+
+    @NotBlank(message = "Every Nurse needs a name!")
+    private String name;
+
+    @NotBlank(message = "Every Nurse needs a city!")
+    private String city;
+
+    public NurseJoy() {
+    }
+
+    public NurseJoy(String username, String password, String name, String city) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.city = city;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "NurseJoy{" + "id=" + id + ", username='" + username + '\'' + ", name='" + name + '\'' +
+                ", city='" + city + '\'' + '}';
+    }
+
+}
