23496a474f99cdcf2748908ee95996c7ec193aed
Miguel Bittar <miguelbittar29@gmail.com>
2025-08-20 11:20:08 -0300

    Initial project setup with Spring Initializr

---
+++ src/main/java/com/hoenn/pokemoncenter/pokemon_center_api/PokemonCenterApiApplication.java
@@ -0,0 +1,13 @@
+package com.hoenn.pokemoncenter.pokemon_center_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonCenterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonCenterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoenn/pokemoncenter/pokemon_center_api/PokemonCenterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoenn.pokemoncenter.pokemon_center_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonCenterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
